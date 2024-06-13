890b8edfd3fd4aa614e348b5b014a48eb60a2fbc
hendisantika <hendisantika@yahoo.co.id>
2024-06-13 15:51:48 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootdatacassandrainvoice/SpringBootDataCassandraInvoiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootdatacassandrainvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataCassandraInvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataCassandraInvoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootdatacassandrainvoice/SpringBootDataCassandraInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootdatacassandrainvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataCassandraInvoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
